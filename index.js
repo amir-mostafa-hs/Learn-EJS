1d68bd38fc7d4274d2b5502edf33704cb22cb4c2
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-06-21 19:14:12 +0430

    add redirect route and code comment

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require("express");
 
+// express app
 const app = express();
 
+// route: / , show home page
 app.get("/", (req, res) => {
   res.sendFile("./views/html/index.html", { root: __dirname });
 });
 
+// route: /about , show about page
 app.get("/about", (req, res) => {
   res.sendFile("./views/html/about.html", { root: __dirname });
 });
 
-app.get("/*", (req, res) => {
+// route: /about-us , redirect to /about
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// route: all ther route , show not found page
+app.get("*", (req, res) => {
   res.sendFile("./views/html/404.html", { root: __dirname });
 });
 
+// app port
 const PORT = 5000;
+// listen for requests
 app.listen(PORT, () => {
   console.log("server run in port: ", PORT);
 });
