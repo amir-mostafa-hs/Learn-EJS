2156715e7f77c72f5448803a7e56e0fd3b2bf80b
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-06-22 09:05:25 +0430

    add code for render ejs

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 const express = require("express");
 
-// express app
+//# express app
 const app = express();
 
-// route: / , show home page
+//# register ejs view engine
+app.set("view engine", "ejs");
+
+//# route: / , show home page
+// app.get("/", (req, res) => {
+//   res.sendFile("./views/html/index.html", { root: __dirname });
+// });
 app.get("/", (req, res) => {
-  res.sendFile("./views/html/index.html", { root: __dirname });
+  res.render("index");
 });
 
-// route: /about , show about page
+//# route: /about , show about page
+// app.get("/about", (req, res) => {
+//   res.sendFile("./views/html/about.html", { root: __dirname });
+// });
 app.get("/about", (req, res) => {
-  res.sendFile("./views/html/about.html", { root: __dirname });
+  res.render("about");
 });
 
-// route: /about-us , redirect to /about
+//# route: /about-us , redirect to /about
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
-// route: all ther route , show not found page
-app.get("*", (req, res) => {
-  res.sendFile("./views/html/404.html", { root: __dirname });
+//# route: all ther route , show not found page
+// app.get("*", (req, res) => {
+//   res.sendFile("./views/html/404.html", { root: __dirname });
+// });
+// app.get("*", (req, res) => {
+//   res.render("404");
+// });
+app.use((req, res) => {
+  res.status(404).render("404");
 });
 
-// app port
+//# app port
 const PORT = 5000;
-// listen for requests
+//# listen for requests
 app.listen(PORT, () => {
   console.log("server run in port: ", PORT);
 });
